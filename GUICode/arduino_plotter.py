96c39078de6771c71be3dca0a57b828155719762
karaind2 <80607570+karaind2@users.noreply.github.com>
2021-03-22 19:17:35 -0400

    Update arduino_plotter.py

--- GUICode/arduino_plotter.py
+++ GUICode/arduino_plotter.py
@@ -28,47 +28,53 @@ y3 = list()
 y4 = list()
 
 i = 0
-ser1 = serial.Serial('COM8', 9600, timeout=0)
-ser2 = serial.Serial('COM9', 9600, timeout=0)
-ser3 = serial.Serial('COM10', 9600, timeout=0)
-ser4 = serial.Serial('COM11', 9600, timeout=0)
+ser1 = serial.Serial('/dev/cu.usbmodem14201', 9600)
+#change the port name to the pilot computer for the final usage of code
+
+
+# ser2 = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=0)
+# ser3 = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=0)
+# ser4 = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=0)
 
 ser1.close()
-ser2.close()
-ser3.close()
-ser4.close()
+# ser2.close()
+# ser3.close()
+# ser4.close()
 ser1.open()
-ser2.open()
-ser3.open()
-ser4.open()
+# ser2.open()
+# ser3.open()
+# ser4.open()
 
 while True:
 
     data1 = ser1.readline()
-    data2 = ser2.readline()
-    data3 = ser3.readline()
-    data4 = ser4.readline()
+    # data2 = ser2.readline()
+    # data3 = ser3.readline()
+    # data4 = ser4.readline()
+    print((data1.decode()))
 
-    plt.subplot(411)
-    plt.plot(i, float(data1.decode()))
-
-    plt.subplot(412)
-    plt.plot(i, float(data2.decode()))
-
-    plt.subplot(413)
-    plt.plot(i, float(data3.decode()))
-
-    plt.subplot(414)
-    plt.plot(i, float(data4.decode()))
-
+    # plt.subplot(411)
+    # plt.plot(i, float(data1.decode()))
+    #
+    # plt.subplot(412)
+    # plt.plot(i, float(data2.decode()))
+    #
+    # plt.subplot(413)
+    # plt.plot(i, float(data3.decode()))
+    #
+    # plt.subplot(414)
+    # plt.plot(i, float(data4.decode()))
+    #
     x1.append(i)
     y1.append(data1.decode())
-    x2.append(i)
-    y2.append(data2.decode())
-    x3.append(i)
-    y3.append(data3.decode())
-    x4.append(i)
-    y4.append(data4.decode())
+    plt.scatter(i,data1.decode())
+    # x2.append(i)
+    # y2.append(data2.decode())
+    # plt.scatter(i,data.decode())
+    # x3.append(i)
+    # y3.append(data3.decode())
+    # x4.append(i)
+    # y4.append(data4.decode())
 
     i += 1
     plt.show()
